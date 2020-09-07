b5859ddcacd669d581d7d2ff6bb73cb656bdfe1f
“EndeZz“ <r-serega13@yandex.ru>
2020-09-07 19:30:41 +0400

    Calculator v2.

--- calc.py
+++ calc.py
@@ -8,39 +8,43 @@ a1, b1, c1 = f1(3,2,6)
 '''
 
 
+def line(a=0, b=0):
+    if not (a == 0):
+        return [-b / a]
+    return []
+
+
 def square(a=0, b=0, c=0):
     if not (a == 0):
         D = b ** 2 - 4 * a * c
         if D > 0:
             x1 = (-b + D ** 0.5) / (2 * a)
             x2 = (-b - D ** 0.5) / (2 * a)
-            return x1, x2
+            return [x1, x2]
         elif D == 0:
             x1 = -b / (2 * a)
-            return x1
-        else:
-            return 'Действительных корней нет'
-
-
-def line(a=0, b=0):
-    if not (a == 0):
-        return -b / a
-    else:
-        return 'Ты придурок!'
+            return [x1]
+    # если уравнение оказалось линейным
+    elif a == 0:
+        return line(b, c)
+    return []
 
 
 def cube(a=0, b=0, c=0, d=0):
+    if a == 0:
+        return square(b, c, d)
+    if a == 0 and b == 0:
+        return line(c, d)
     if not (a == 0) and (d == 0):
         dis = -4 * b ** 3 * d + b ** 2 * c ** 2 - 4 * a * c ** 3 + 18 * a * b * c * d - 27 * a ** 2 * d ** 2
         if dis > 0:
             x1 = 0
             x2 = (-b + d ** 0.5) / (2 * a)
             x3 = (-b - d ** 0.5) / (2 * a)
-            return x1, x2, x3
+            return [x1, x2, x3]
         if dis == 0:
             x2 = -b / (2 * a)
-            return x2
-
+            return [x2]
     p = (3 * a * c - b ** 2) / (3 * a ** 2)
     q = (2 * b ** 3 - 9 * a * b * c + 27 * a ** 2 * d) / (27 * a ** 3)
     S = (4 * (3 * a * c - b ** 2) ** 3 + (2 * b ** 3 - 9 * a * b * c + 27 * a ** 2 * d) * (
@@ -59,8 +63,8 @@ def cube(a=0, b=0, c=0, d=0):
             x3 = 2 * (-p / 3) ** 0.5 * m.cos((t + 4 * m.pi) / 3) - b / (3 * a)
             if q == 0:
                 x3 = -b / (3 * a)
-            return x1, x2, x3
-    if S > 0:
+            return [x1, x2, x3]
+    elif S > 0:
         if (-q / 2 + S ** 0.5) > 0:
             y1 = m.exp(m.log(m.fabs(-q / 2 + S ** 0.5)) / 3)
         if (-q / 2 + S ** 0.5) < 0:
@@ -78,8 +82,8 @@ def cube(a=0, b=0, c=0, d=0):
         x1 = y1 + y2 - b / a / 3
         x2 = complex(Re1, Im1)
         x3 = complex(Re1, -Im1)
-        return x1, x2, x3
-    if S == 0:
+        return [x1, x2, x3]
+    elif S == 0:
         if q < 0:
             y1 = m.exp(m.log(m.fabs(-q / 2)) / 3)
         if q > 0:
@@ -89,7 +93,12 @@ def cube(a=0, b=0, c=0, d=0):
         x1 = 2 * y1 - b / a / 3
         x2 = -y1 - b / a / 3
         x3 = -y1 - b / a / 3
-        return x1, x2, x3
+        return [x1, x2, x3]
+    return []
 
 
+print(line(45, 9))
+print(square(2, 45, 9))
+print(cube(0, 2, 45, 9))
+print(cube(0, 0, 0, 0))
 print(cube(1, 6, -15, 22))  # С комплексными корнями
